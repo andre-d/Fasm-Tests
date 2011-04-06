40c1d63ec0a09da04762c32f1785374996b2ce01
Andre D <andre@andred.ca>
2011-04-06 19:33:18 -0400

    First commit

---
+++ numberascii.asm
@@ -0,0 +1,41 @@
+
+; Convert a number to a string
+
+format ELF executable 3
+entry start
+
+segment readable executable
+
+	CFG_NUMBER = 103
+	CFG_ANOTHER_NUMBER = 18721
+
+exit:
+	; Call to exit the program
+	mov	eax,1   ; sys_exit
+	xor	ebx,ebx ; error code 0
+	int	0x80    ; kick the kernel
+
+printmsg:
+	; Prints a message
+	; ecx: char * buffer
+	; edx: int size
+	push eax 
+	push ebx
+	mov	eax,4 ; sys_write
+	mov	ebx,1 ; file descriptor
+	int	0x80  ; kick the kernel
+	pop ebx
+	pop eax
+	ret
+
+start:
+	mov	ecx,msg      ; Place our message in ecx
+	mov	edx,msg_size ; Place the size in edx
+	call printmsg    ; Print the message
+	jmp exit         ; Now exit cleanly
+	
+
+segment readable writeable
+
+msg db 'Simple demo to convert an integer to a string',0xA
+msg_size = $-msg
